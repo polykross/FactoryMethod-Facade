3ac1adb8b7ca0496916b6a07f18be136f89cd33f
Kyrylo Vasylenko <vasilenkowm@gmail.com>
2019-02-24 13:52:28 +0200

    Add Benchmarker header file
    Signed-off-by: Kyrylo Vasylenko <vasilenkowm@gmail.com>

---
+++ FactoryMethod/Projects/Logistics/src/benchmark/Benchmarker.h
@@ -0,0 +1,20 @@
+#ifndef BENCHMARKER_H_
+#define BENCHMARKER_H_
+
+#include "../logistic/Logistics.h"
+#include "../storage/Storage.h"
+#include "../Destination.h"
+#include "../Cargo.h"
+
+class Benchmarker {
+public:
+	Benchmarker(Cargo& cargo, Logistics& logistics, Destination& destination, Storage<Transport>& storage);
+	void deliver();
+private:
+	Cargo& _cargo;
+	Logistics& _logistics;
+	Destination& _destination;
+	Storage<Transport>& _storage;
+};
+
+#endif // BENCHMARKER_H_
